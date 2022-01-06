1886f02f656e5ba26a893fa4a7777df347587074
Ralf Kotulla <ralf.kotulla@gmail.com>
2022-01-06 09:43:55 -0600

    minor changes

--- rssnir__fit_nonlinearity.py
+++ rssnir__fit_nonlinearity.py
@@ -9,9 +9,13 @@ if __name__ == "__main__":
 
     fn = sys.argv[1]
     rss = rss_reduce.RSS(fn)
-    rss.reduce(write_dumps=False)
+    # rss.reduce(write_dumps=False)
     # rss.write_results()
-    rss.fit_nonlinearity(ref_frame_id=10)
+
+    rss.load_all_files()
+    # rss.subtract_first_read()
+
+    rss.fit_nonlinearity(ref_frame_id=4, make_plot=False)
 
     # rss.plot_pixel_curve(818,1033)
     # rss.plot_pixel_curve(1700,555)
