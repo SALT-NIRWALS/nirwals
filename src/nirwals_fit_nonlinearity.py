d4eb9d3d54f682c668f665c1b97c7e73ff54e2e2
Ralf Kotulla <ralf.kotulla@gmail.com>
2023-06-28 12:55:59 -0500

    improved command line argument parsing

--- src/nirwals_fit_nonlinearity.py
+++ src/nirwals_fit_nonlinearity.py
@@ -2,20 +2,39 @@
 
 import sys
 import os
+import multiparlog as mplog
+import argparse
+import logging
 
 from nirwals import NIRWALS
 
 
 if __name__ == "__main__":
 
-    fn = sys.argv[1]
+    mplog.setup_logging(debug_filename="debug.log",
+                        log_filename="run_analysis.log")
+    mpl_logger = logging.getLogger('matplotlib')
+    mpl_logger.setLevel(logging.WARNING)
 
-    try:
-        saturation_fn = sys.argv[2]
-    except:
-        saturation_fn = 62000
+    logger = logging.getLogger("NirwalsFitNonlinearity")
 
-    rss = NIRWALS(fn, saturation=saturation_fn)
+    cmdline = argparse.ArgumentParser()
+    cmdline.add_argument("--maxfiles", dest="max_number_files", default=None, type=int,
+                         help="limit number of files to load for processing")
+    cmdline.add_argument("--nonlinearity", dest="nonlinearity_fn", type=str, default=None,
+                         help="non-linearity correction coefficients (3-d FITS cube)")
+    cmdline.add_argument("--saturation", dest="saturation", default=62000,
+                         help="saturation value/file")
+    cmdline.add_argument("--refpixel", dest="use_ref_pixels", default=False, action='store_true',
+                         help="use reference pixels [default: NO]")
+    cmdline.add_argument("files", nargs="+",
+                         help="list of input filenames")
+    args = cmdline.parse_args()
+
+    fn = args.files[0]
+    saturation_fn = args.saturation
+    rss = NIRWALS(fn, saturation=saturation_fn,
+                  max_number_files=args.max_number_files)
 
     # rss.reduce(write_dumps=False)
     # rss.write_results()
