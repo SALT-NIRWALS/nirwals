1acabb250bbdcd8d267182fa5b6aaa939186a1bc
Ralf Kotulla <ralf.kotulla@gmail.com>
2023-12-05 16:31:56 -0600

    some refactoring of the pairwise fitting algorithm

--- src/nirwals/__init__.py
+++ src/nirwals/__init__.py
@@ -1,4 +1,5 @@
-from .nirwals import  NIRWALS,dump_options,darktype_GOOD,darktype_COLD,darktype_WARM,darktype_HOT
+from .nirwals import  NIRWALS,dump_options,darktype_GOOD,darktype_COLD,darktype_WARM,darktype_HOT, \
+    fit_pairwise_slope_samples,select_samples
 from .provenance import DataProvenance
 from .previous_file_picker import  PreviousFilePicker
 from .refpixel_calibrate import reference_pixels_to_background_correction
