a4c5b23dc54b8ebd35feb09f8d5909dbfd5798d5
Ralf Kotulla <ralf.kotulla@gmail.com>
2024-01-18 11:24:35 -0600

    added installation of watchdog as standalone command line tool

--- src/nirwals/__init__.py
+++ src/nirwals/__init__.py
@@ -3,3 +3,5 @@ from .nirwals import  NIRWALS,dump_options,darktype_GOOD,darktype_COLD,darktype_
 from .provenance import DataProvenance
 from .previous_file_picker import  PreviousFilePicker
 from .refpixel_calibrate import reference_pixels_to_background_correction
+
+from .nirwals_watchdog import NirwalsOnTheFlyReduction
